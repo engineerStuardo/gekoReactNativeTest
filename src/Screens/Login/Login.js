e5336cb0da669d2359c5f829ee01d0c06940f1cc
icortez <icortez@ufg.edu.sv>
2021-06-15 09:44:03 -0600

    fixing problem in login

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Image } from 'react-native';
+import { Image, ScrollView } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 import { LoginHeader } from '../../Shared/loginHeader';
@@ -16,63 +16,65 @@ const Login = ({ navigation }) => {
   const [password, setPassword] = useState('');
 
   return (
-    <LoginContainer>
-      <LoginHeader />
-      <BoxContainer
-        style={{
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 0,
-            height: 2,
-          },
-          shadowOpacity: 0.23,
-          shadowRadius: 2.62,
-          elevation: 4,
-        }}
-      >
-        <Image source={require('../../../assets/logoGeko.png')} />
-        <UserInput
-          label='User'
-          value={user}
-          textContentType='username'
-          keyboardType='default'
-          autoCapitalize='none'
-          onChangeText={value => setUser(value)}
-          theme={{ colors: { primary: '#0073b1' } }}
-          left={
-            <TextInput.Icon
-              style={{ marginTop: 5 }}
-              name='account'
-              color={'gray'}
-            />
-          }
-        />
-        <PasswordInput
-          label='Password'
-          value={password}
-          textContentType='password'
-          keyboardType='default'
-          autoCapitalize='none'
-          secureTextEntry={true}
-          onChangeText={value => setPassword(value)}
-          theme={{ colors: { primary: '#0073b1' } }}
-          left={
-            <TextInput.Icon
-              style={{ marginTop: 5 }}
-              name='lock'
-              color={'gray'}
-            />
-          }
-        />
-
-        <LoginButton
-          mode='contained'
-          onPress={() => navigation.navigate('Historical')}
+    <ScrollView>
+      <LoginContainer>
+        <LoginHeader />
+        <BoxContainer
+          style={{
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+            elevation: 4,
+          }}
         >
-          Login
-        </LoginButton>
-      </BoxContainer>
-    </LoginContainer>
+          <Image source={require('../../../assets/logoGeko.png')} />
+          <UserInput
+            label='User'
+            value={user}
+            textContentType='username'
+            keyboardType='default'
+            autoCapitalize='none'
+            onChangeText={value => setUser(value)}
+            theme={{ colors: { primary: '#0073b1' } }}
+            left={
+              <TextInput.Icon
+                style={{ marginTop: 5 }}
+                name='account'
+                color={'gray'}
+              />
+            }
+          />
+          <PasswordInput
+            label='Password'
+            value={password}
+            textContentType='password'
+            keyboardType='default'
+            autoCapitalize='none'
+            secureTextEntry={true}
+            onChangeText={value => setPassword(value)}
+            theme={{ colors: { primary: '#0073b1' } }}
+            left={
+              <TextInput.Icon
+                style={{ marginTop: 5 }}
+                name='lock'
+                color={'gray'}
+              />
+            }
+          />
+
+          <LoginButton
+            mode='contained'
+            onPress={() => navigation.navigate('Historical')}
+          >
+            Login
+          </LoginButton>
+        </BoxContainer>
+      </LoginContainer>
+    </ScrollView>
   );
 };
 
